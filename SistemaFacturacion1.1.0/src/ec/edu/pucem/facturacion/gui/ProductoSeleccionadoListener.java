2ce2a0576b80ce7a0310e6f97e35d3abb531d76c
amesitos <161412285+amesitos@users.noreply.github.com>
2024-07-17 01:40:34 +0200

    Delete SistemaFacturacion1.1.0 directory

--- SistemaFacturacion1.1.0/src/ec/edu/pucem/facturacion/gui/ProductoSeleccionadoListener.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.pucem.facturacion.gui;
-
-import ec.edu.pucem.facturacion.models.Producto;
-
-// Interfaz con función vacia que almacena una instancia de Producto
-public interface ProductoSeleccionadoListener {
-    void productoSeleccionado(Producto producto);
-}
